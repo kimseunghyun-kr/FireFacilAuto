e6c2bf95478b6700b03fafec06de2612b89a7e27
kimseunghyun-kr <incongnito12@gmail.com>
2024-01-31 01:23:54 +0900

    fixed error when no return value with custom json deserializer -> refix

--- FireFacilAuto/src/main/java/com/FireFacilAuto/domain/DTO/api/Items.java
+++ FireFacilAuto/src/main/java/com/FireFacilAuto/domain/DTO/api/Items.java
@@ -2,7 +2,10 @@ package com.FireFacilAuto.domain.DTO.api;
 
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
 
+import java.util.Collections;
 import java.util.List;
 
 @Data
@@ -11,5 +14,11 @@ public class Items {
 
     private List<ApiResponseItem> item;
 
+    public Items() {
+    }
 
+
+    public Items(List<ApiResponseItem> objects) {
+        this.item = objects;
+    }
 }
--- FireFacilAuto/src/main/java/com/FireFacilAuto/domain/DTO/api/ItemsDeserializer.java
+++ FireFacilAuto/src/main/java/com/FireFacilAuto/domain/DTO/api/ItemsDeserializer.java
@@ -1,6 +1,5 @@
 package com.FireFacilAuto.domain.DTO.api;
 
-
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonNode;
@@ -8,24 +7,30 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
+
+import static java.util.Objects.isNull;
 
 public class ItemsDeserializer extends StdDeserializer<Items> {
+
     public ItemsDeserializer() {
-        this(null);
-    }
-    protected ItemsDeserializer(Class<?> vc) {
-        super(vc);
+        super(Items.class);
     }
 
     @Override
-    public Items deserialize(JsonParser jp, DeserializationContext deserializationContext) throws IOException {
-        JsonNode node = jp.getCodec().readTree(jp);
+    public Items deserialize(JsonParser p, DeserializationContext ctxt) throws IOException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        JsonNode node = p.getCodec().readTree(p);
+        JsonNode itemNode = node.findValue("item");
 
-        if (node.isTextual() && node.textValue().isEmpty()) {
-            return new Items(); // Create an empty ItemBasket object
+        if (isNull(itemNode)) {
+            return new Items(Collections.emptyList());
         } else {
-            return new ObjectMapper().convertValue(node, Items.class);
+            List<ApiResponseItem> itemList = Arrays.stream(objectMapper.treeToValue(itemNode, ApiResponseItem[].class)).toList();
+            return new Items(itemList);
         }
     }
 }
