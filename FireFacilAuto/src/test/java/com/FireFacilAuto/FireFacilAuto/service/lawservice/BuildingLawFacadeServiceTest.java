9f30b137a55a482897e5ddf5323fd30adca3ad0b
kimseunghyun-kr <incongnito12@gmail.com>
2024-02-27 09:22:35 +0900

    groundwork laid for the junit mokito testing. to create further functionality to generate object results for further testing .
    todo
    
    1. AOP pointcut validation server side and repository for the results

---
+++ FireFacilAuto/src/test/java/com/FireFacilAuto/FireFacilAuto/service/lawservice/BuildingLawFacadeServiceTest.java
@@ -0,0 +1,115 @@
+package com.FireFacilAuto.FireFacilAuto.service.lawservice;
+
+import com.FireFacilAuto.FireFacilAuto.service.lawservice.ObjectBuilder.Building.BuildingAttributes;
+import com.FireFacilAuto.FireFacilAuto.service.lawservice.ObjectBuilder.Building.TestBuildingObjectBuilder;
+import com.FireFacilAuto.FireFacilAuto.service.lawservice.ObjectBuilder.Building.TestFloorObjectBuilder;
+import com.FireFacilAuto.FireFacilAuto.service.lawservice.ObjectBuilder.Law.TestBuildingLawObjectBuilder;
+import com.FireFacilAuto.FireFacilAuto.service.lawservice.ObjectBuilder.Law.TestFloorLawObjectBuilder;
+import com.FireFacilAuto.domain.Conditions;
+import com.FireFacilAuto.domain.entity.building.Building;
+import com.FireFacilAuto.domain.entity.building.Floor;
+import com.FireFacilAuto.domain.entity.lawfields.BuildingLawFields;
+import com.FireFacilAuto.domain.entity.lawfields.FloorLawFields;
+import com.FireFacilAuto.domain.entity.results.ResultSheet;
+import com.FireFacilAuto.service.lawService.BuildingAndFloorLawExecutionFacadeService;
+import com.FireFacilAuto.service.lawService.LawService;
+import lombok.extern.slf4j.Slf4j;
+import org.hibernate.query.sqm.ComparisonOperator;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
+@Slf4j
+public class BuildingLawFacadeServiceTest {
+    @InjectMocks
+    private final BuildingAndFloorLawExecutionFacadeService lawExecutionFacadeService;
+
+    @MockBean
+    private LawService lawService;
+
+
+    private final TestBuildingObjectBuilder buildingGenerator = new TestBuildingObjectBuilder();
+    private final TestFloorObjectBuilder floorObjectBuilder = new TestFloorObjectBuilder();
+    private final TestBuildingLawObjectBuilder buildingLawObjectBuilder = new TestBuildingLawObjectBuilder();
+    private final TestFloorLawObjectBuilder floorLawObjectBuilder = new TestFloorLawObjectBuilder();
+
+    public BuildingLawFacadeServiceTest(BuildingAndFloorLawExecutionFacadeService lawExecutionFacadeService) {
+        this.lawExecutionFacadeService = lawExecutionFacadeService;
+    }
+
+    @Test
+    public void testBuilding2Test() {
+        BuildingLawFields fireExtinguisherGFAMoreThan33 = buildingLawObjectBuilder.singularBuildingLawBuilder(-1,-1,
+                1,1,33,null,-1,-1,-1,-1, null);
+        List<Conditions> COList1 = List.of(buildingLawObjectBuilder.conditionBuilder(fireExtinguisherGFAMoreThan33,"GFA", ComparisonOperator.GREATER_THAN_OR_EQUAL));
+        fireExtinguisherGFAMoreThan33.setConditionsList(COList1);
+
+        BuildingLawFields AutoFireAlarmOgfloorsMTOE6 = buildingLawObjectBuilder.singularBuildingLawBuilder(-1,-1,2,1,-1,null,-1,-1,6,-1,null);
+        List<Conditions> COList2 = List.of(buildingLawObjectBuilder.conditionBuilder(AutoFireAlarmOgfloorsMTOE6, "overgroundFloors", ComparisonOperator.GREATER_THAN_OR_EQUAL));
+        AutoFireAlarmOgfloorsMTOE6.setConditionsList(COList2);
+
+        FloorLawFields areaSumClinicGreaterThan600 = floorLawObjectBuilder.singularFloorLawBuilder(2,3,1,6,-1,null,-1,-1.0,600,null,null);
+        List<Conditions> COList3 = List.of(floorLawObjectBuilder.conditionBuilder(areaSumClinicGreaterThan600,"floorAreaSum",ComparisonOperator.GREATER_THAN_OR_EQUAL));
+        areaSumClinicGreaterThan600.setConditionsList(COList3);
+
+        BuildingAttributes ba = new BuildingAttributes(100,2,3,1,1,6,1500, LocalDate.now(), true);
+        Building building = buildingGenerator.buildingObjectBuilder(ba);
+
+        List<Floor> floorList = List.of(floorObjectBuilder.generateSingleFloor(building,2,3,1,true,200,1),
+                floorObjectBuilder.generateSingleFloor(building,2,3,1,false,300,1),
+                floorObjectBuilder.generateSingleFloor(building,2,3,2,false,200,1),
+                floorObjectBuilder.generateSingleFloor(building,2,3,3,false,200,1),
+                floorObjectBuilder.generateSingleFloor(building,2,1,4,false,200,1),
+                floorObjectBuilder.generateSingleFloor(building,2,1,5,false,200,1),
+                floorObjectBuilder.generateSingleFloor(building,2,1,6,false,200,1)
+        );
+
+        building.setCompositeFloors(floorList);
+
+        MockitoAnnotations.openMocks(this);
+
+        // Given
+        List<BuildingLawFields> candidateBuildingLaw = new ArrayList<>(List.of(new BuildingLawFields[]{fireExtinguisherGFAMoreThan33, AutoFireAlarmOgfloorsMTOE6}));
+        List<FloorLawFields> candidateFloorLaw = new ArrayList<>(List.of(new FloorLawFields[]{areaSumClinicGreaterThan600}));
+
+        // Mock the behavior of the lawService -> getLaws
+        when(lawService.getLawsWithApplicablePurpose(building)).thenReturn(candidateBuildingLaw);
+        for (int i = 0; i < 4; i++) {
+            when(lawService.getLawsWithApplicablePurpose(floorList.get(i))).thenReturn(candidateFloorLaw);
+        }
+        // Act
+        ResultSheet resultSheet = lawExecutionFacadeService.executeLaw(building);
+
+        log.info("resultSheet : {}", resultSheet);
+//        resultsheet not null
+        assertThat(resultSheet).isNotNull();
+//        all resultsheet fireExtinguisherInstall true
+        assertThat(resultSheet.getFloorResultsList())
+                .extracting(floorResults -> floorResults.getExtinguisherInstallation().getExtinguisherApparatus())
+                .containsOnly(true);
+//      all alarmDevice autofiredetector true
+        assertThat(resultSheet.getFloorResultsList())
+                .extracting(floorResults -> floorResults.getAlarmDeviceInstallation().getAutoFireDectionApparatus())
+                .containsOnly(true);
+
+//        only floors B1 - 3 has sprinkler
+        for (int i = 0; i < 4; i++) {
+            assertThat(resultSheet.getFloorResultsList().get(i).getExtinguisherInstallation().getSprinklerApparatus())
+                    .isEqualTo(true);
+        }
+//      4 onwards has null -> no need to install
+        assertThat(resultSheet.getFloorResultsList().get(4).getExtinguisherInstallation().getSprinklerApparatus())
+                .isNull();
+
+    }
+}
